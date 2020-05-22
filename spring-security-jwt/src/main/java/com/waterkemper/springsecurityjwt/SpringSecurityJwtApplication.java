9fe5e2738f8e6f7a151927b44bafca4c8f0e30fe
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2020-05-21 22:47:54 -0300

    Created configuration classes for authentication and authorization

---
+++ spring-security-jwt/src/main/java/com/waterkemper/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,15 @@
+package com.waterkemper.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
