247bdaa856263ace8daa38c275dc541adf4b152e
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2020-06-30 20:36:54 -0300

    Create a endpoint for return a file text, and another to write file directly in HttpServletResponse

--- spring-security-jwt/waterk-sprint-file-upload/src/main/java/com/waterkemper/UploadController.java
+++ spring-security-jwt/waterk-sprint-file-upload/src/main/java/com/waterkemper/UploadController.java
@@ -1,15 +1,19 @@
 package com.waterkemper;
 
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 
 @RestController
 @RequestMapping("/v1/api/")
@@ -49,5 +53,30 @@ public class UploadController {
                 .contentType(MediaType.parseMediaType(contentType))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
+    }
+
+    @GetMapping("download/textfile")
+    public ResponseEntity<Resource> download(HttpServletRequest servletRequest) throws IOException {
+        String contentType = MediaType.TEXT_PLAIN_VALUE;
+        Resource resource = new ByteArrayResource("Testando escrita de arquivo texto".getBytes(Charset.defaultCharset()));
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"textfile.txt\"")
+                .body(resource);
+    }
+
+
+    @GetMapping("download/writeservletresponse")
+    public void testing(HttpServletResponse response) throws IOException {
+        response.getOutputStream().write("Testando envio direto".getBytes());
+            response.setContentType("text/plain");
+        response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"writeatservlet.txt\"");
+        response.flushBuffer();
+    }
+
+    @GetMapping("nocontent")
+    public ResponseEntity<String> notFoundContent() {
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 }
