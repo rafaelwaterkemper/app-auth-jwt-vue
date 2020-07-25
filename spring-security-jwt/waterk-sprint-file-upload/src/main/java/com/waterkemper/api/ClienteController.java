3fbed24104a6f1b0de9694102a9fdfec93cf8713
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2020-07-25 01:00:30 -0300

    Tests with ObjectMapper and @RequestBody

--- spring-security-jwt/waterk-sprint-file-upload/src/main/java/com/waterkemper/api/ClienteController.java
+++ spring-security-jwt/waterk-sprint-file-upload/src/main/java/com/waterkemper/api/ClienteController.java
@@ -5,6 +5,10 @@ import com.waterkemper.repositories.ClienteRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @RestController
@@ -12,13 +16,22 @@ import java.util.List;
 public class ClienteController {
 
     private final ClienteRepository clienteRepository;
+    private final EntityManager entityManager;
 
-    public ClienteController(ClienteRepository clienteRepository) {
+    public ClienteController(ClienteRepository clienteRepository, EntityManager entityManager) {
         this.clienteRepository = clienteRepository;
+        this.entityManager = entityManager;
     }
 
     @PostMapping
     public Cliente save(@RequestBody Cliente cliente) {
+        System.out.println("name");
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> query = criteriaBuilder.createQuery(Cliente.class);
+        Root<Cliente> from = query.from(Cliente.class);
+        CriteriaQuery<Cliente> select = query.select(from);
+        entityManager.createQuery(select).getResultList();
+
         return clienteRepository.save(cliente);
     }
 
--- spring-security-jwt/waterk-sprint-file-upload/src/main/java/com/waterkemper/api/GerenteController.java
+++ spring-security-jwt/waterk-sprint-file-upload/src/main/java/com/waterkemper/api/GerenteController.java
@@ -1,10 +1,15 @@
 package com.waterkemper.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.waterkemper.model.Gerente;
 import com.waterkemper.repositories.GerenteRepository;
 import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @RestController
@@ -12,14 +17,29 @@ import java.util.List;
 public class GerenteController {
 
     private final GerenteRepository gerenteRepository;
+    private final EntityManager entityManager;
+    private final ObjectMapper mapper;
 
-    public GerenteController(GerenteRepository gerenteRepository) {
+    public GerenteController(GerenteRepository gerenteRepository, EntityManager entityManager, ObjectMapper mapper) {
         this.gerenteRepository = gerenteRepository;
+        this.entityManager = entityManager;
+        this.mapper = mapper;
     }
 
     @PostMapping
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Gerente save(@RequestBody Gerente gerente) {
-        return gerenteRepository.save(gerente);
+        entityManager.persist(gerente);
+        entityManager.flush();
+        return gerente;
+    }
+
+    @PostMapping("/mapper")
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Gerente saveMapper(@RequestBody String gerente) throws JsonProcessingException {
+        Gerente gerente1 = mapper.readValue(gerente, Gerente.class);
+        entityManager.persist(gerente1);
+        return gerente1;
     }
 
     @GetMapping
